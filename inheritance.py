18251b0b686c8ee83aa2cdf1c394dfeec9df9f66
Raju Dhakal <wrazu19@gmail.com>
2021-09-09 15:38:23 +0545

    Polymorphism

--- inheritance.py
+++ inheritance.py
@@ -1,3 +1,27 @@
+# class CloudFactoryWorker:
+#     def __init__(self, name, l_name, project, employee_id):
+#         self.first_name = name
+#         self.last_name = l_name
+#         self.project_name = project
+#         self.id = employee_id
+
+#     def calculate_salary(self, hours):
+#         salary = hours * 20
+#         return salary
+
+# class CFWorker(CloudFactoryWorker):
+#     pass
+
+
+# class CoreTeam(CloudFactoryWorker):
+#     pass
+
+# CF1 = CFWorker('Raju', 'Dhakal', 'Expensify', 102563)
+# print(CF1.first_name)
+
+
+# POLYMORPHISM
+
 class CloudFactoryWorker:
     def __init__(self, name, l_name, project, employee_id):
         self.first_name = name
@@ -5,12 +29,20 @@ class CloudFactoryWorker:
         self.project_name = project
         self.id = employee_id
 
+    def calculate_salary(self):
+        print('I am based class')
+
 class CFWorker(CloudFactoryWorker):
-    pass
+    def calculate_salary(self):
+        print('I am hourly bases worker')
 
 
 class CoreTeam(CloudFactoryWorker):
-    pass
+    def calculate_salary(self):
+        print('I am paid based on title.')
+
 
 CF1 = CFWorker('Raju', 'Dhakal', 'Expensify', 102563)
-print(CF1.first_name)
+CF2 = CoreTeam('Raju', 'Dhakal', 'Expensify', 102563)
+CF1.calculate_salary()
+CF2.calculate_salary()
